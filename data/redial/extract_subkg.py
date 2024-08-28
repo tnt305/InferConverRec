3720ef2336a14dc82053eb9b997b7a8894d75f41
Thien Dang <110881337+tnt305@users.noreply.github.com>
2024-08-28 08:37:56 +0700

    Update extract_subkg.py

--- data/redial/extract_subkg.py
+++ data/redial/extract_subkg.py
@@ -4,7 +4,7 @@ import pickle as pkl
 from tqdm.auto import tqdm
 # import os
 # os.chdir("data/redial")
-current_dir = "/redial"
+current_dir = "data/redial"
 
 def get_item_set(file):
     entity = set()

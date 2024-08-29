84de6a34aa8f4dc8876503beda4add64b7c308bb
Thien Dang <110881337+tnt305@users.noreply.github.com>
2024-08-29 09:37:38 +0700

    Update infer_conv.py

--- src/infer_conv.py
+++ src/infer_conv.py
@@ -45,10 +45,10 @@ def parse_args():
     parser.add_argument("--n_prefix_conv", type=int)
     parser.add_argument("--num_bases", type=int, default=8, help="num_bases in RGCN")
     # optim
-    parser.add_argument("--num_train_epochs", type=int, default= 6, help="Total number of training epochs to perform.")
+    parser.add_argument("--num_train_epochs", type=int, default= 8, help="Total number of training epochs to perform.")
     parser.add_argument("--max_train_steps", type=int, default=None,
                         help="Total number of training steps to perform. If provided, overrides num_train_epochs.")
-    parser.add_argument("--per_device_train_batch_size", type=int, default=4,
+    parser.add_argument("--per_device_train_batch_size", type=int, default= 32,
                         help="Batch size (per device) for the training dataloader.")
     parser.add_argument("--per_device_eval_batch_size", type=int, default=4,
                         help="Batch size (per device) for the evaluation dataloader.")

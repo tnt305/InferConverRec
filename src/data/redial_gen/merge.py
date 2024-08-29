d585e8384ffd8c201a0feed3687d12d33efcdceb
Thien Dang <110881337+tnt305@users.noreply.github.com>
2024-08-29 15:24:45 +0700

    Update merge.py

--- src/data/redial_gen/merge.py
+++ src/data/redial_gen/merge.py
@@ -5,7 +5,7 @@ parser = ArgumentParser()
 parser.add_argument("--gen_file_prefix", type=str, required=True)
 args = parser.parse_args()
 gen_file_prefix = args.gen_file_prefix
-dataset = 'inspired'
+dataset = 'redial'
 
 for split in ['train', 'valid', 'test']:
     raw_file_path = f"../{dataset}/{split}_data_processed.jsonl"

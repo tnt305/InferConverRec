29cdb4c2fddd065a1f2da0e50fec51a8b7ccf717
Thien Dang <110881337+tnt305@users.noreply.github.com>
2024-08-29 09:59:52 +0700

    Update infer_conv.py

--- src/infer_conv.py
+++ src/infer_conv.py
@@ -49,7 +49,7 @@ def parse_args():
     parser.add_argument("--num_train_epochs", type=int, default= 8, help="Total number of training epochs to perform.")
     parser.add_argument("--max_train_steps", type=int, default=None,
                         help="Total number of training steps to perform. If provided, overrides num_train_epochs.")
-    parser.add_argument("--per_device_train_batch_size", type=int, default= 8,
+    parser.add_argument("--per_device_train_batch_size", type=int, default= 4,
                         help="Batch size (per device) for the training dataloader.")
     parser.add_argument("--per_device_eval_batch_size", type=int, default=4,
                         help="Batch size (per device) for the evaluation dataloader.")

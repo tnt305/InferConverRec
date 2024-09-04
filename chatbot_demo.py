b7bb2a0e7bd346b3240bf4df8f205763c8faeb8c
Thien Dang <110881337+tnt305@users.noreply.github.com>
2024-09-04 14:38:02 +0700

    Update chatbot_demo.py

--- chatbot_demo.py
+++ chatbot_demo.py
@@ -20,8 +20,8 @@ text_tokenizer = AutoTokenizer.from_pretrained("FacebookAI/roberta-base")
 text_tokenizer.add_special_tokens({
     'additional_special_tokens': ['<movie>'],
 })
-text_encoder.resize_token_embeddings(384)
 text_encoder = AutoModel.from_pretrained("FacebookAI/roberta-base").to(device)
+text_encoder.resize_token_embeddings(384)
 
 # Load and inspect your pre-trained prompt encoder
 pre_trained_prompt_state = torch.load("/kaggle/working/InferConverRec/src/output_dir/dialogpt_prompt-pre_prefix-20_redial/best/model.pt", map_location=device)
